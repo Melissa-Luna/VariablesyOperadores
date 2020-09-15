674b4b83e496ca928fcd069f4f0279e841e8fdc4
Melissa <melisaluna21@outlook.com>
2020-09-14 19:41:15 -0500

    Initial commit

---
+++ src/com/melissaluna/java/Main.java
@@ -0,0 +1,34 @@
+package com.melissaluna.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int X = 7;
+        System.out.println("X=" +X);
+        int Y = 3;
+        System.out.println("Y=" +Y);
+
+        // suma
+        int sum = X + Y;
+        System.out.println("Suma:" +sum);
+
+        // resta
+        int res = X - Y;
+        System.out.println("Resta:" +res);
+
+        //multiplicacion
+        int mul = X * Y;
+        System.out.println("Multiplicacion:" +mul);
+
+        // division
+        int div = X/Y;
+        System.out.println("Division:" +div);
+
+        // modulo (residuo division)
+        int mod = X % Y;
+        System.out.println("Modulo:" +mod);
+
+    }
+}
